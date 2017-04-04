7b6adb50caaa7bb1e1c4d75bf0f7ed9f2f2be2f4
Luis Meier <meierluis@hotmail.de>
2017-04-04 11:00:17 +0200

    Quick Fix
    Catkin Make was not compiling irb2400. deleted unused ABB files

--- catkin_ws/build/ur_kinematics/catkin_generated/setup_cached.sh
+++ catkin_ws/build/ur_kinematics/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/luis/VT2/catkin_ws/devel/.private/ur_kinematics:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/luis/VT2/catkin_ws/devel/.private/ur_kinematics/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/luis/VT2/catkin_ws/devel/.private/ur_kinematics/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/luis/VT2/catkin_ws/build/ur_kinematics"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/luis/VT2/catkin_ws/devel/.private/ur_kinematics/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/luis/VT2/catkin_ws/src/universal_robot/ur_kinematics:$ROS_PACKAGE_PATH"
