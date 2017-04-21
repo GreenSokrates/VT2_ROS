6e582c7b7727feaacd235a6a5a9bf24160346469
Luis Meier <meierluis@hotmail.de>
2017-04-21 17:10:52 +0200

    WIP
    Compilierbar, jedoch macht Planer nicht das was er soll. Visualisierung des Planners funktioniert auch nicht.

--- catkin_ws/src/cell_core/src/planning.cpp
+++ catkin_ws/src/cell_core/src/planning.cpp
@@ -6,11 +6,11 @@
 
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
+#include <moveit_msgs/DisplayTrajectory.h>
 
 
 using namespace ros;
 using namespace moveit;
-using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -19,9 +19,8 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
+  // Setting up movegroup named "manipulator"
   static const std::string PLANNING_GROUP = "manipulator";
-
-  // Setting up movegroup
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
@@ -36,203 +35,155 @@ int main(int argc, char **argv)
   
   // Defining target_pose1
   geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.w = 1.0;
-  target_pose1.position.x = 0.28;
-  target_pose1.position.y = -0.7;
-  target_pose1.position.z = 1.0;
+  target_pose1.orientation.x = 1.0;
+  target_pose1.orientation.y = 0.0;
+  target_pose1.orientation.z = 0.0;
+  target_pose1.orientation.w = 0.0;
+  target_pose1.position.x = 1.2;
+  target_pose1.position.y = 0.7;
   move_group.setPoseTarget(target_pose1);
 
-  // Call the Planner, compute and visualize
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
   bool success = move_group.plan(my_plan);
 
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+   ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+   /* Sleep to give Rviz time to visualize the plan. */
+   sleep(5.0);
+
+   std::vector<double> group_variable_values;
+   move_group.getCurrentState()->copyJointGroupPositions(move_group.getCurrentState()->getRobotModel()->getJointModelGroup(move_group.getName()), group_variable_values);
+
+   group_variable_values[0] = -1.0;
+   move_group.setJointValueTarget(group_variable_values);
+   success = move_group.plan(my_plan);
+
+   ROS_INFO("Visualizing plan 2 (joint space goal) %s",success?"":"FAILED");
+   /* Sleep to give Rviz time to visualize the plan. */
+   sleep(5.0);
+
+   // Doing this with path constraints (?)
+   // Setting orientation constraint
+   moveit_msgs::OrientationConstraint ocm;
+   ocm.link_name = "link_6";
+   ocm.header.frame_id = "base_link";
+   ocm.orientation.w = -0.5;
+   ocm.orientation.x =  0.5;
+   ocm.orientation.y = -0.5;
+   ocm.orientation.z =  0.5;
+   ocm.absolute_x_axis_tolerance = 0.1;
+   ocm.absolute_y_axis_tolerance = 0.1;
+   ocm.absolute_z_axis_tolerance = 0.1;
+   ocm.weight = 1.0;
+   // add to constraint list
+   moveit_msgs::Constraints test_constraints;
+   test_constraints.orientation_constraints.push_back(ocm);
+   // Using previous joint target
+   move_group.setPathConstraints(test_constraints);
+   move_group.setPoseTarget(target_pose1);
+
+   success = move_group.plan(my_plan);
+
+   // let's never speak of this again
+   move_group.clearPathConstraints();
+
+   ROS_INFO("Visualizing plan 3 (constraints) %s",success?"":"FAILED");
+   /* Sleep to give Rviz time to visualize the plan. */
+   sleep(10.0);
+
+   // Linear paths are specified using waypoints
+   // make waypoints
+   std::vector<geometry_msgs::Pose> waypoints;
+
+   geometry_msgs::PoseStamped temp = move_group.getCurrentPose("tool0");
+   geometry_msgs::Pose target_pose3 = temp.pose;
+   target_pose3.position.x -= 0.2;
+   target_pose3.position.z -= 0.2;
+   waypoints.push_back(target_pose3);  // up and out
+
+   target_pose3.position.y -= 0.2;
+   waypoints.push_back(target_pose3);  // left
+
+   target_pose3.position.z += 0.2;
+   target_pose3.position.y += 0.2;
+   target_pose3.position.x += 0.2;
+   waypoints.push_back(target_pose3);  // down and right (back to start)
+
+   // Make path with points at .01m steps
+   moveit_msgs::RobotTrajectory trajectory;
+   double fraction = move_group.computeCartesianPath(waypoints,
+                                                0.01,  // eef_step
+                                                0.0,   // jump_threshold
+                                                trajectory);
+
+   ROS_INFO("Visualizing plan 4 (cartesian path) (%.2f%% acheived)",
+               fraction * 100.0);
+   /* Sleep to give Rviz time to visualize the plan. */
+   sleep(15.0);
+
+   moveit_msgs::CollisionObject collision_object;
+   collision_object.header.frame_id = move_group.getPlanningFrame();
+
+   /* The id of the object is used to identify it. */
+   collision_object.id = "box1";
+
+   /* Define a box to add to the world. */
+   shape_msgs::SolidPrimitive primitive;
+   primitive.type = primitive.BOX;
+   primitive.dimensions.resize(3);
+   primitive.dimensions[0] = 0.4;
+   primitive.dimensions[1] = 0.1;
+   primitive.dimensions[2] = 0.4;
+
+   /* A pose for the box (specified relative to frame_id) */
+   geometry_msgs::Pose box_pose;
+   box_pose.orientation.w = 1.0;
+   box_pose.position.x =  0.6;
+   box_pose.position.y = -0.4;
+   box_pose.position.z =  1.2;
+
+   collision_object.primitives.push_back(primitive);
+   collision_object.primitive_poses.push_back(box_pose);
+   collision_object.operation = collision_object.ADD;
+
+   std::vector<moveit_msgs::CollisionObject> collision_objects;
+   // add to list of collision objects
+   collision_objects.push_back(collision_object);
+   ROS_INFO("Add an object into the world");
+   planning_scene_interface.addCollisionObjects(collision_objects);
+
+   /* Sleep so we have time to see the object in RViz */
+   sleep(2.0);
+
+   move_group.setPlanningTime(10.0);
+
+   move_group.setStartState(*move_group.getCurrentState());
+   move_group.setPoseTarget(target_pose1);
+   success = move_group.plan(my_plan);
+
+   ROS_INFO("Visualizing plan 5 (pose goal move around box) %s",
+           success?"":"FAILED");
+   /* Sleep to give Rviz time to visualize the plan. */
+   sleep(10.0);
+
+   // Attach to robot
+   ROS_INFO("Attach the object to the robot");
+   move_group.attachObject(collision_object.id);
+   /* Sleep to give Rviz time to show the object attached (different color). */
+   sleep(4.0);
+
+   ROS_INFO("Detach the object from the robot");
+   move_group.detachObject(collision_object.id);
+   /* Sleep to give Rviz time to show the object detached. */
+   sleep(4.0);
+
+   ROS_INFO("Remove the object from the world");
+   std::vector<std::string> object_ids;
+   object_ids.push_back(collision_object.id);
+   planning_scene_interface.removeCollisionObjects(object_ids);
+   /* Sleep to give Rviz time to show the object is no longer there. */
+   sleep(4.0);
 
 
-  // Uncomment below line when working with a real robot
-  // move_group.move();
-
-  // Planning to a joint-space goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Let's set a joint space goal and move towards it.  This will replace the
-  // pose target we set above.
-  //
-  // To start, we'll create an pointer that references the current robot's state.
-  // RobotState is the object that contains all the current position/velocity/acceleration data.
-  moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-  //
-  // Next get the current set of joint values for the group.
-  std::vector<double> joint_group_positions;
-  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
-
-  // Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
-  joint_group_positions[0] = -1.0;  // radians
-  move_group.setJointValueTarget(joint_group_positions);
-
-  success = move_group.plan(my_plan);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
-
-    // Planning with Path Constraints
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Path constraints can easily be specified for a link on the robot.
-  // Let's specify a path constraint and a pose goal for our group.
-  // First define the path constraint.
-  moveit_msgs::OrientationConstraint ocm;
-  ocm.link_name = "tool0";
-  ocm.header.frame_id = "base_link";
-  ocm.orientation.w = 1.0;
-  ocm.absolute_x_axis_tolerance = 0.1;
-  ocm.absolute_y_axis_tolerance = 0.1;
-  ocm.absolute_z_axis_tolerance = 0.1;
-  ocm.weight = 1.0;
-
-  // Now, set it as the path constraint for the group.
-  moveit_msgs::Constraints test_constraints;
-  test_constraints.orientation_constraints.push_back(ocm);
-  move_group.setPathConstraints(test_constraints);
-
-  // We will reuse the old goal that we had and plan to it.
-  // Note that this will only work if the current state already
-  // satisfies the path constraints. So, we need to set the start
-  // state to a new pose.
-  robot_state::RobotState start_state(*move_group.getCurrentState());
-  geometry_msgs::Pose start_pose2;
-  start_pose2.orientation.w = 1.0;
-  start_pose2.position.x = 0.55;
-  start_pose2.position.y = -0.05;
-  start_pose2.position.z = 0.8;
-  start_state.setFromIK(joint_model_group, start_pose2);
-  move_group.setStartState(start_state);
-
-  // Now we will plan to the earlier pose target from the new
-  // start state that we have just created.
-  move_group.setPoseTarget(target_pose1);
-
-  // Planning with constraints can be slow because every sample must call an inverse kinematics solver.
-  // Lets increase the planning time from the default 5 seconds to be sure the planner has enough time to succeed.
-  move_group.setPlanningTime(10.0);
-
-  success = move_group.plan(my_plan);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 3 (constraints) %s", success ? "" : "FAILED");
-
-  // When done with the path constraint be sure to clear it.
-  move_group.clearPathConstraints();
-
-  // Cartesian Paths
-  // ^^^^^^^^^^^^^^^
-  // You can plan a cartesian path directly by specifying a list of waypoints
-  // for the end-effector to go through. Note that we are starting
-  // from the new start state above.  The initial pose (start state) does not
-  // need to be added to the waypoint list but adding it can help with visualizations
-  std::vector<geometry_msgs::Pose> waypoints;
-  waypoints.push_back(start_pose2);
-
-  geometry_msgs::Pose target_pose3 = start_pose2;
-
-  target_pose3.position.z += 0.2;
-  waypoints.push_back(target_pose3);  // up 
-
-  target_pose3.position.y -= 0.1;
-  waypoints.push_back(target_pose3);  // left
-
-  target_pose3.position.z -= 0.2;
-  target_pose3.position.y += 0.2;
-  target_pose3.position.x -= 0.2;
-  waypoints.push_back(target_pose3);  // down and right 
-
-  // Cartesian motions are frequently needed to be slower for actions such as approach and retreat
-  // grasp motions. Here we demonstrate how to reduce the speed of the robot arm via a scaling factor
-  // of the maxiumum speed of each joint. Note this is not the speed of the end effector point.
-  move_group.setMaxVelocityScalingFactor(0.1);
-
-  // We want the cartesian path to be interpolated at a resolution of 1 cm
-  // which is why we will specify 0.01 as the max step in cartesian
-  // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
-  // Warning - disabling the jump threshold while operating real hardware can cause
-  // large unpredictable motions of redundant joints and could be a safety issue
-  moveit_msgs::RobotTrajectory trajectory;
-  const double jump_threshold = 0.0;
-  const double eef_step = 0.01;
-  double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 4 (cartesian path) (%.2f%% acheived)", fraction * 100.0);
-
-
-  // Adding/Removing Objects and Attaching/Detaching Objects
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Define a collision object ROS message.
-  moveit_msgs::CollisionObject collision_object;
-  collision_object.header.frame_id = move_group.getPlanningFrame();
-
-  // The id of the object is used to identify it.
-  collision_object.id = "box1";
-
-  // Define a box to add to the world.
-  shape_msgs::SolidPrimitive primitive;
-  primitive.type = primitive.BOX;
-  primitive.dimensions.resize(3);
-  primitive.dimensions[0] = 0.4;
-  primitive.dimensions[1] = 0.1;
-  primitive.dimensions[2] = 0.4;
-
-  //Define a pose for the box (specified relative to frame_id)
-  geometry_msgs::Pose box_pose;
-  box_pose.orientation.w = 1.0;
-  box_pose.position.x = 0.6;
-  box_pose.position.y = -0.4;
-  box_pose.position.z = 1.2;
-
-  collision_object.primitives.push_back(primitive);
-  collision_object.primitive_poses.push_back(box_pose);
-  collision_object.operation = collision_object.ADD;
-
-  std::vector<moveit_msgs::CollisionObject> collision_objects;
-  collision_objects.push_back(collision_object);
-
-  // Now, let's add the collision object into the world
-  ROS_INFO_NAMED("tutorial", "Add an object into the world");
-  planning_scene_interface.addCollisionObjects(collision_objects);
-
-  // Sleep to allow MoveGroup to recieve and process the collision object message
-  ros::Duration(1.0).sleep();
-
-  // Now when we plan a trajectory it will avoid the obstacle
-  move_group.setStartState(*move_group.getCurrentState());
-  move_group.setPoseTarget(target_pose1);
-
-  success = move_group.plan(my_plan);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 5 (pose goal move around cuboid) %s", success ? "" : "FAILED");
-
-
-  // Now, let's attach the collision object to the robot.
-  ROS_INFO_NAMED("tutorial", "Attach the object to the robot");
-  move_group.attachObject(collision_object.id);
-
-
-  /* Sleep to allow MoveGroup to recieve and process the attached collision object message */
-  ros::Duration(1.0).sleep();
-
-  // Now, let's detach the collision object from the robot.
-  ROS_INFO_NAMED("tutorial", "Detach the object from the robot");
-  move_group.detachObject(collision_object.id);
-
-
-  /* Sleep to allow MoveGroup to recieve and process the detach collision object message */
-  ros::Duration(1.0).sleep();
-
-  // Now, let's remove the collision object from the world.
-  ROS_INFO_NAMED("tutorial", "Remove the object from the world");
-  std::vector<std::string> object_ids;
-  object_ids.push_back(collision_object.id);
-  planning_scene_interface.removeCollisionObjects(object_ids);
-
-  /* Sleep to give Rviz time to show the object is no longer there.*/
-  ros::Duration(1.0).sleep();
-
-
-  ros::shutdown();
-  return 0;
+   spinner.stop();
+   return(0);
 }
