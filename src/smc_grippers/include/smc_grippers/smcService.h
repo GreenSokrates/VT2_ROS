d0e62dbccaeb0c46ef98c6823ab2682e074e5c44
Unknown <meierluis@hotmail.de>
2017-07-31 15:29:00 +0200

    Finished

--- src/smc_grippers/include/smc_grippers/smcService.h
+++ src/smc_grippers/include/smc_grippers/smcService.h
@@ -10,29 +10,28 @@
 
 // includes vor The services
 #include <smc_grippers/gripper_service.h>
-//#include <smc_grippers/press_service.h>
-//#include <smc_grippers/3Bgripper_service.h>
-
-
+#include <smc_grippers/press_service.h>
+#include <smc_grippers/Bgripper_service.h>
 
 class smcService
 {
-  protected:
-    ros::NodeHandle nh;
-    /*
+protected:
+  ros::NodeHandle nh;
+  /*
     ros::Service gripper;
     ros::Service press;
-    ros::Service 3Bgripper;*/
-  private:
-    SMCServoJXCE1 *gripper;
+    ros::Service Bgripper;*/
+private:
+  //SMCServoJXCE1 *gripper;
+  //SMCServoJXCE1 *press;
+  //SMCServoJXCE1 *Bgripper;
 
-  public:
-    smcService();
-    virtual ~smcService();
-    bool gripperCallback(smc_grippers::gripper_service::Request &req, smc_grippers::gripper_service::Response &res);
-  
-   /* bool pressCallback(smc_grippers::press_service::Request &req, smc_grippers::press_service::Response &res);
-    bool 3BgripperCallback(smc_grippers::3Bgripper_service::Request &req, smc_grippers::3Bgripper_service::Response &res);*/
+public:
+  smcService();
+  virtual ~smcService();
+  bool gripperCallback(smc_grippers::gripper_service::Request &req, smc_grippers::gripper_service::Response &res);
+  bool pressCallback(smc_grippers::press_service::Request &req, smc_grippers::press_service::Response &res);
+  bool BgripperCallback(smc_grippers::Bgripper_service ::Request &req, smc_grippers::Bgripper_service ::Response &res);
 };
 
 #endif /* SMC_SERVICE */
--- src/smc_grippers/src/smcService.cpp
+++ src/smc_grippers/src/smcService.cpp
@@ -7,16 +7,34 @@ smcService::smcService()
 smcService::~smcService()
 {
 }
-
+/* 
 bool smcService::gripperCallback(smc_grippers::gripper_service::Request &req, smc_grippers::gripper_service::Response &res)
 {
     gripper->enable();
     int pos = req.position;
-
     gripper->setTargetPosition(500);
     gripper->disable();
     return 1;
 }
+
+bool smcService::pressCallback(smc_grippers::press_service::Request &req, smc_grippers::press_service::Response &res)
+{
+    press->enable();
+    int pos = req.position;
+    press->setTargetPosition(500);
+    press->disable();
+    return 1;
+}
+
+bool smcService::BgripperCallback(smc_grippers::Bgripper_service::Request &req, smc_grippers::Bgripper_service::Response &res)
+{
+    Bgripper->enable();
+    int pos = req.position;
+    Bgripper->setTargetPosition(500);
+    Bgripper->disable();
+    return 1;
+} */
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "smcService");
@@ -25,11 +43,24 @@ int main(int argc, char **argv)
     spinner.start();
 
     smcService ss;
-    ros::ServiceServer gripperService = nh.advertiseService("gripper_service", &smcService::gripperCallback, &ss);
+    // Advertising all Services
+   /*  ros::ServiceServer gripperService = nh.advertiseService("/smcService/gripper", &smcService::gripperCallback, &ss);
+    ros::ServiceServer BgripperService = nh.advertiseService("/smcService/3bGripper", &smcService::BgripperCallback, &ss);
+    ros::ServiceServer pressService = nh.advertiseService("/smcService/press", &smcService::pressCallback, &ss); */
 
-    EtherCAT etherCAT("10.0.0.205");
+    // Establish ethercat Communication
+    EtherCAT etherCAT("10.0.0.2");
     CoE coe(etherCAT, 0.005);
-    SMCServoJXCE1 gripper(etherCAT, coe, 0x0000);
+    try
+    {
+        SMCServoJXCE1 gripper(etherCAT, coe, 0x0000);
+       /*  SMCServoJXCE1 press(etherCAT, coe, 0xFFFF);
+        SMCServoJXCE1 Bgripper(etherCAT, coe, 0xFFFE); */
+    }
+    catch (exception &e)
+    {
+        std::cout << e.what() << endl;
+    }
 
     ros::waitForShutdown();
 
